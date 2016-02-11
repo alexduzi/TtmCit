e00e9a9d42cd455863baa00902ad961cecae1978
alexduzi <duzihd@gmail.com>
2016-02-11 14:39:02 -0200

    json example to set the divergence

--- ttms.py
+++ ttms.py
@@ -1,38 +1,76 @@
 import datetime
 import string
 import json
+import urllib2
+import unicodedata
 
+#02/01/2016
+DATETIME_FORMAT = '%d/%m/%Y %H:%M'
 
-DATETIME_FORMAT = '%Y/%m/%d %H:%M'
-
+class Divergence(object):
+    """docstring for Divergence"""
+    def __init__(self, code_divergence, name_divergence, acronym_divergence, number_order):
+        self.code_divergence = int(code_divergence)
+        self.name_divergence = name_divergence
+        self.acronym_divergence = acronym_divergence
+        self.number_order = number_order
+        print self.number_order
+        
+    def __str__(self):
+        return "{} {} {} {}".format(self.code_divergence,
+                                    self.name_divergence,
+                                    self.acronym_divergence,
+                                    self.number_order)
+                                                           
+    def to_dict(self):
+        return { "codeDivergence": self.code_divergence, 
+                 "nameDivergence": self.name_divergence, 
+                 "acronymDivergence": self.acronym_divergence,
+                 "numberOrder": self.number_order}                                                                                                                                      
+    
 
 class Ttm(object):
     """docstring for Ttm"""
-    def __init__(self, date, check):
-        super(Ttm, self).__init__()
+    def __init__(self, date, divergence):
         self.date = date
-        self.check_type = 'Check-in' if int(check) == 1 else 'Check-out'
+        self.divergence = divergence
         self.comment = 'Esqueci'
 
 
     def __str__(self):
         return "{} {} {}".format(datetime.datetime.strftime(self.date, DATETIME_FORMAT),
-                                 self.check_type,
+                                 self.divergence,
                                  self.comment)
 
 
 def get_ttms(dates):
-    return [Ttm(datetime.datetime.strptime(date.strip().split('|')[0], DATETIME_FORMAT), date.strip().split('|')[1]) for date in dates]
+    return [Ttm(datetime.datetime.strptime(date.strip().split('|')[0], DATETIME_FORMAT), 
+                Divergence(date.split('|')[1], 
+                           date.split('|')[2], 
+                           date.split('|')[3],
+                           date.split('|')[4])) 
+            for date in dates]
 
 
 def send_ttm_to_server(ttm):
-    print "Sending: {}".format(ttm)
+        print "Sending: {}".format(ttm)
+        ttm_dict = { 
+                     "dateOccurrence" : datetime.datetime.strftime(ttm.date, DATETIME_FORMAT),
+					 "divergence" : ttm.divergence.to_dict(),
+					 "textEmployeeComment" : ttm.comment
+				   }
+        # req = urllib2.Request('http://ttm.ciandt.com/occurrence')
+        # req.add_header('Content-Type', 'application/json')
+        # response = urllib2.urlopen(req, json.dumps(ttm_dict))
+        print json.dumps(ttm_dict)
+        
 
 
 def main():
     dates_from_file = []
     with open('hours.txt', 'r') as fh:
-        dates_from_file = fh.readlines()
+        for line in fh.readlines():
+            dates_from_file.append(line.strip())
 
     ttms = get_ttms(dates_from_file)
     for ttm in ttms:
